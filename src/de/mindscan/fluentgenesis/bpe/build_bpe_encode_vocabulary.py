5122ab22a7a793c3ea857f52a19f44760afea2d6
mindscan-de <maxim.gansert@gmail.com>
2019-10-27 20:57:04 +0100

    add removal of tokens containing asian characters

--- src/de/mindscan/fluentgenesis/bpe/build_bpe_encode_vocabulary.py
+++ src/de/mindscan/fluentgenesis/bpe/build_bpe_encode_vocabulary.py
@@ -330,23 +330,77 @@ def find_word_containing( pair, current_token_map ):
     pass
 
 def emit_obvious_ascii_tokens(token_map):
-    for char in range(32, 255):
+    # range is 32 to 256-1
+    for char in range(32, 256):
         emit_token(chr(char))
 
+cjk_ranges = [
+        ( 0x4E00,  0x62FF),
+        ( 0x6300,  0x77FF),
+        ( 0x7800,  0x8CFF),
+        ( 0x8D00,  0x9FCC),
+        ( 0x3400,  0x4DB5),
+        (0x20000, 0x215FF),
+        (0x21600, 0x230FF),
+        (0x23100, 0x245FF),
+        (0x24600, 0x260FF),
+        (0x26100, 0x275FF),
+        (0x27600, 0x290FF),
+        (0x29100, 0x2A6DF),
+        (0x2A700, 0x2B734),
+        (0x2B740, 0x2B81D),
+        (0x2B820, 0x2CEAF),
+        (0x2CEB0, 0x2EBEF),
+        (0x2F800, 0x2FA1F)
+    ]
+
+def is_cjk(char):
+    char = ord(char)
+    for bottom, top in cjk_ranges:
+        if char >= bottom and char <= top:
+            return True
+    return False
+        
+def contains_asian_chars(key):
+    for c in key:
+        if is_cjk(c):
+            return True
+    return False
+        
+def remove_tokens_containing_asian_chars(current_token_map):
+    keys_to_remove = []
+    for key in current_token_map.keys():
+        if contains_asian_chars(key):
+            keys_to_remove.append(key)
+             
+    if len(keys_to_remove) >0:
+        for key in keys_to_remove:
+            del current_token_map[key]
+            
+    return current_token_map
+
 def build_dictionary(hparams, token_map):
-    # emit all one element tokens
-    # create a copy of the  
-    current_token_map=rebuild_token_map(token_map)
+    current_token_map = rebuild_token_map(token_map)
     #print (str(current_token_map).encode("utf-8"))
     
     print("the whole dictionary has now length : " + str(len(current_token_map)))
-    
-    emit_obvious_ascii_tokens(current_token_map)
-    emit_complete_tokens(current_token_map)
-    current_token_map = remove_completed_tokens(current_token_map)
-    # print (str(current_token_map).encode("utf-8"))
+    print("removing tokens containing asian characters")
+    # remove asian tokens, because they cause the tokens to inflate too much, and I do not have enough training data for these "rare" tokens
+    # the asian characters alone will clog up the entire available dictionary
+    current_token_map = remove_tokens_containing_asian_chars(current_token_map)
     
     print("the whole dictionary has now length : " + str(len(current_token_map)))
+    print("emitting ascii and complete tokens")
+
+    # emit all (important) one element tokens
+    emit_obvious_ascii_tokens(current_token_map)
+    emit_complete_tokens(current_token_map)
+    
+    current_token_map = remove_completed_tokens(current_token_map)
+    
+    print("the whole dictionary has now length : " + str(len(current_token_map)))
+    
+    # print (str(current_token_map).encode("utf-8"))
     
     ## FOR - number of iterations / or there is no most probable lexeme anymore (count of lexems is one)
     for i in range(hparams['tokens_to_emit']):
