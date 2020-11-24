7a9f194820ba26f83f2f9c7b29cf7a923363b8f2
mindscan-de <maxim.gansert@gmail.com>
2020-11-24 20:28:34 +0100

    provide more backend functions. returns success for each post operation

--- src/de/mindscan/cheaplithium/httpserver/cheap_lithium_dev_server.py
+++ src/de/mindscan/cheaplithium/httpserver/cheap_lithium_dev_server.py
@@ -29,7 +29,7 @@ import sys
 sys.path.insert(0,'../../../../../src')
 
 import json
-import uuid
+import uuid as uid
 import os.path
 
 from fastapi import FastAPI, Form
@@ -39,25 +39,74 @@ app = FastAPI()
 DATAMODEL_DIR = '../../../../../data/cheaplithium/dm/'
 
 
+
+# DecisionModel "Database"
+# persist only as long as the server is runing, for development, we don't need database support 
+decisionModelDatabase = {}
+
+# -----------------------------------------
+# Later extract the decision modelling code
+# -----------------------------------------
+def create_successful_uuid_result(uuid:str):
+    return {"uuid":uuid, "isSuccess":True, "isError":False}
+
+# --------------------------------------
+
+
+
+# --------------------------------------
+# API-Webserver "code" - 
+# --------------------------------------
+
 @app.get("/")
 def read_root():
     return {"message":"Hello World! It works! But now, go away!"}
 
-@app.get("/getDecisionModel/{unique_id}")
-async def provide_decision_model( unique_id:str='0518f24f-41a0-4f13-b5f6-94a015b5b04c'):
-    dmdict ={}
-    read_uuid = uuid.UUID('{' + unique_id + '}')
+@app.get("/CheapLithium/rest/getDecisionModel/{uuid}")
+async def provide_decision_model( uuid:str='0518f24f-41a0-4f13-b5f6-94a015b5b04c'):
+    try:
+        read_uuid = uid.UUID('{' + uuid + '}')
+    except:
+        return {"messsage":"invalid uuid"}
     
-    if ( str(read_uuid) == unique_id):
+    if ( str(read_uuid) == uuid):
         jsonfilepath = DATAMODEL_DIR + str(read_uuid) + '.json'
+        
         if os.path.isfile(jsonfilepath):
             with open(jsonfilepath) as json_source_file:
-                dmdict = json.load(json_source_file)
+                return json.load(json_source_file)
         else:
-            abs_name = os.path.abspath(jsonfilepath)
-            dmdict=  {"message":"no_such_file "+str(abs_name)}
+            # try to load model from hash-map - for "updated" models then load from hashmap first.
+            if uuid in decisionModelDatabase:
+                return decisionModelDatabase[uuid]
+            else:
+                abs_name = os.path.abspath(jsonfilepath)
+                return {"message":"no_such_file "+str(abs_name)}
     else:
-        dmdict=  {"message":"uid doesn't match."}
+        return  {"message":"uuid doesn't match."}
     
-    return dmdict
+    return {}
+
+@app.post("/CheapLithium/rest/createDecisionModel")
+async def create_decision_model( name:str = Form(...), displayname:str=Form(...), 
+    description:str=Form(...), version:str = Form(...)):
     
+    uuid = uid.uuid4()
+    # create a model with a start and an end node, using generated model uuid
+    
+    return create_successful_uuid_result(uuid)
+
+
+@app.post("/CheapLithium/rest/updateDecisionModel")
+async def update_decision_model(uuid: str = Form(...), name:str = Form(...),  displayname:str=Form(...), 
+    description:str=Form(...), version:str = Form(...)):
+    # update that model in the dictionary... - no persistence required
+    return create_successful_uuid_result(uuid)
+
+
+@app.post("/CheapLithium/rest/cloneDecisionModel")
+async def clone_decision_model(uuid: str = Form(...)):
+    # clone/copy that model, but create different uuids for each element
+    # clone that model in the dictionary... - no persistence required 
+    
+    return create_successful_uuid_result(uuid)
